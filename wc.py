0191cd96182f5eaf97f24e18f693ecd83c4383b9
olaperswe <49958203+olaperswe@users.noreply.github.com>
2019-11-09 22:32:25 +0100

    Add files via upload
    Added stopowords

--- wc.py
+++ wc.py
@@ -5,21 +5,28 @@ Created on Mon Oct 21 21:11:39 2019
 @author: Ola
 """
 
-filename=input("Filename:")
+stopwords_eng=["the","and","to","in","of", "from","on","for","as","by","at"]
+stopwords_swe=["att", "är", "och","en","det","som","ett","detta","inte","på",
+          "med","om","de","jag","den","av","till","för","eller","för",
+          "man","han","men","så","då","ni","har","ska","vi","i"]
 
-fp=open(filename)
-
-di=dict()
-
-for line in fp :
-    line=line.rstrip()
+def filetodict(fp,di,stopwords) :
+   """Read a file and populate dictionary with the words, exclude stopwords """
+   for line in fp :
+#    line=line.rstrip()
     wds=line.split()
     for w in wds:
-        if w in di:
-            di[w]=di[w]+1
-        else:
-            di[w] = 1
+        if w not in stopwords:
+            if w in di:
+                di[w]=di[w]+1
+            else:
+                di[w] = 1 
 
-sortedByNumberDict = sorted(di.items(),key=lambda t:t[1])
-
-print (sortedByNumberDict)
+filename=input("Filename:")
+fp=open(filename)
+if fp != 0:
+    di=dict()
+    filetodict(fp,di,stopwords_swe)
+    sortedByNumberDict = sorted(di.items(),key=lambda t:t[1])
+    n=min(len(sortedByNumberDict),5)
+    print (sortedByNumberDict[-n:])
